84f03ada425b30de09b51b5f3e99203a6f35889b
Ragueel <kayne787@gmail.com>
2021-12-20 22:00:26 +0600

    Code refactored

--- Project/Assets/Shooter/Scripts/Observations/Rewards.cs
+++ Project/Assets/Shooter/Scripts/Observations/Rewards.cs
@@ -4,5 +4,8 @@
     {
         public const float HitReward = 0.15f;
         public const float KillReward = 1f;
+        public const float ShootCorrectReward = 0.05f;
+        public const float ShootIncorrectPenalty = -0.005f;
+        public const float TimeoutPenalty = -0.1f;
     }
 }
